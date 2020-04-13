45ef394e3df6f45e2e3619b42f6266aadea09042
Enrico Franke <enni2002@gmx.de>
2020-04-13 16:43:00 +0200

    added pause command not Working

---
+++ src/de/nirvanacw/commands/PauseTimer.java
@@ -0,0 +1,17 @@
+package de.nirvanacw.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PauseTimer implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+
+
+        return true;
+    }
+}
