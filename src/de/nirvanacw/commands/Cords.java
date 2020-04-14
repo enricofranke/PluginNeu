02691b400a6165e81358ba8d4ea98b46a64d5858
Enrico Franke <enni2002@gmx.de>
2020-04-14 23:20:42 +0200

    changes some fixes

--- src/de/nirvanacw/commands/Cords.java
+++ src/de/nirvanacw/commands/Cords.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Cords implements CommandExecutor {
@@ -24,7 +25,11 @@ public class Cords implements CommandExecutor {
                 }
             }
             if (args.length == 1){
-                String add = (ChatColor.GOLD+ args[0] +ChatColor.WHITE+"|| "+ChatColor.RED+" x: "+ player.getLocation().getX()+ChatColor.WHITE+" || "+ ChatColor.GREEN+" y: " + player.getLocation().getY() +ChatColor.WHITE+" || "+ChatColor.BLUE+" z: " + player.getLocation().getZ());
+                double x = player.getLocation().getX();
+                double y = player.getLocation().getY();
+                double z = player.getLocation().getZ();
+                DecimalFormat format = new DecimalFormat("#.##");
+                String add = (ChatColor.GOLD+ args[0] +ChatColor.WHITE+" || "+ChatColor.RED+" x: "+format.format(x)+ChatColor.WHITE+" || "+ ChatColor.GREEN+" y: " + format.format(y) +ChatColor.WHITE+" || "+ChatColor.BLUE+" z: " + format.format(z));
                 Bukkit.broadcastMessage(add);
                 places.add(add);
             }
